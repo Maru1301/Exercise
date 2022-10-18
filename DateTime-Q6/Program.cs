6a18d099ced0652e23463432932e6646d5b19863
Maru1301 <how7633818@gmail.com>
2022-10-18 22:35:19 +0800

    DateTime

---
+++ DateTime-Q6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Q6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("請輸入日期: ");
+			string input = Console.ReadLine();
+			if (string.IsNullOrEmpty(input))
+			{
+				Console.WriteLine("請輸入正確日期");
+				return;
+			}
+
+			bool isDate = DateTime.TryParse(input, out DateTime dt);
+
+			int day = dt.Day;
+
+			if(day <= 10)
+			{
+				Console.WriteLine("上旬");
+			}
+			else if(day<= 20)
+			{
+				Console.WriteLine("中旬");
+			}
+			else
+			{
+				Console.WriteLine("下旬");
+			}
+		}
+	}
+}
---
+++ DateTime-Q7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Q7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime now = DateTime.Now;
+			Console.WriteLine($"現在時間 {now}");
+
+			int hour = now.Hour;
+
+			if(hour <= 12)
+			{
+				Console.WriteLine("早安");
+			}
+			else if(hour <= 18)
+			{
+				Console.WriteLine("午安");
+			}
+			else
+			{
+				Console.WriteLine("晚安");
+			}
+		}
+	}
+}
