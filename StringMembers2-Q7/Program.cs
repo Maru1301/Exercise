ce209659cee0d9317bc5af047a10c38310bb97c3
Maru1301 <how7633818@gmail.com>
2022-10-18 23:01:09 +0800

    StringMembers2

---
+++ StringMembers2-Q7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMembers2_Q7
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("請輸入兩個含有小數點的數: ");
+			bool isDecimal1 = decimal.TryParse(Console.ReadLine(), out decimal num1), 
+				isDecimal2 = decimal.TryParse(Console.ReadLine(), out decimal num2);
+
+			decimal sum = num1 + num2;
+
+			string result = sum.ToString("#.##");
+
+			Console.WriteLine(result);
+		}
+	}
+}
---
+++ StringMembers2-Q8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMembers2_Q8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("請輸入你的手機號碼: ");
+			string input = Console.ReadLine();
+			if(input.Length != 10)
+			{
+				Console.WriteLine("錯誤的手機號碼");
+				return;
+			}
+
+			string start = input.Substring(0, 4);
+			string end = input.Substring(8, 2);
+			string stars = new string('*', 4);
+
+			string result = start + stars + end;
+
+			Console.WriteLine(result);
+		}
+	}
+}
