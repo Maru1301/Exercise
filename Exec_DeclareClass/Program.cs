68ee3cc6cfdde6d90bfb33ff8423672cd8b8bb96
Maru1301 <how7633818@gmail.com>
2022-10-21 19:09:12 +0800

    Class

--- Exec_DeclareClass/Program.cs
+++ Exec_DeclareClass/Program.cs
@@ -30,27 +30,75 @@ namespace Exec_DeclareClass
 
 	class Member
 	{
-		public string Name { get; set; }
-		public string Account { get; set; }
-		public string Password { get; set; }
-		public string Email { get; set; }
-		public Address Address { get; set; }
-		public string Cellphone { get; set; }
-		public Telephone Telephone { get; set; }
-
-		public void Register(string Name, string Account, string Password, string cofirmPassword, string Email)
-		{
-
+		private string _Name;
+		public string Name 
+		{ 
+			get => _Name; 
+			set => _Name = value; 
+		}
+		private string _Account;
+		public string Account 
+		{ 
+			get => _Account;
+			set => _Account = value; 
+		}
+		private string _Password;
+		public string Password 
+		{ 
+			get => _Password; 
+			set => _Password = value; 
+		}
+		private string _Email;
+		public string Email 
+		{ 
+			get => _Email; 
+			set => _Email = value; 
+		}
+		private Address _Address;
+		public Address Address 
+		{ 
+			get => _Address; 
+			set => _Address = value; 
+		}
+		private string _Cellphone;
+		public string Cellphone 
+		{ 
+			get => _Cellphone; 
+			set => _Cellphone = value; 
+		}
+		private Telephone _Telephone;
+		public Telephone Telephone 
+		{ 
+			get => _Telephone; 
+			set => _Telephone = value; 
 		}
 
-		public void ForgetPassword(string Name, string Email)
+		public void Register(string Name, string Account, string Password, string confirmPassword, string Email)
 		{
-
+			if(Password != confirmPassword)
+			{
+				throw new Exception("Your password and confirm password are not the same!");
+			}
+				this.Name = Name;
+				this.Account = Account;
+				this.Password = Password;
+				this.Email = Email;
 		}
 
-		public void Authenticate(string Account, string Password)
+		public string ForgetPassword(string Name, string Email)
 		{
+			if (Name != this.Name) throw new Exception("Wrong name!");
+			if (Email != this.Email) throw new Exception("Wrong e-mail!");
 
+			return Password;
+		}
+
+		public bool Authenticate(string Account, string Password)
+		{
+			if (Account != this.Account) throw new Exception("Wrong account");
+			if (Password != this.Password) throw new Exception("Wrong Password");
+
+			return true;
 		}
 	}
 }
